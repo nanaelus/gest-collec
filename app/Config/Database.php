b2c458d384fa648c8d880f8baf4f6ea5485e8f26
nanaelus <anael.arnaud@laposte.net>
2024-11-06 17:58:33 +0100

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,8 +29,8 @@ class Database extends Config
     public array $development = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => 'root',
+        'username'     => '',
+        'password'     => '',
         'database'     => 'gest-collec',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
@@ -45,6 +45,11 @@ class Database extends Config
         'failover'     => [],
         'port'         => 3306,
         'numberNative' => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
     ];
 
     /**
@@ -73,6 +78,34 @@ class Database extends Config
         'port'        => 3306,
         'foreignKeys' => true,
         'busyTimeout' => 1000,
+    ];
+
+    public array $tests = [
+        'DSN'         => '',
+        'hostname'    => 'localhost',
+        'username'    => 'root',
+        'password'    => 'root',
+        'database'    => 'test',
+        'DBDriver'    => 'MySQLi',
+        'DBPrefix'    => '',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI
+        // DEVS
+        'pConnect'    => false,
+        'DBDebug'     => true,
+        'charset'     => 'utf8',
+        'DBCollat'    => '',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
+        'dateFormat'  => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
     ];
 
     public function __construct()
