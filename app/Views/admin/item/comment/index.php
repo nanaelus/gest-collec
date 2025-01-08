1c36589fc2d8f83afd571dec63d2c71485395705
nanaelus <anael.arnaud@laposte.net>
2025-01-08 11:20:36 +0100

    Profile page (Front) Fixed date/comments

--- app/Views/admin/item/comment/index.php
+++ app/Views/admin/item/comment/index.php
@@ -55,7 +55,7 @@
                     sortable : false,
                     render : function(data, type, row) {
                         return (row.deleted_at === null ?
-                            `<a title="Désactiver le commentaire" href="<?= base_url('admin/comment/desactivate/'); ?>${row.id}"><i class="fa-solid fa-xl fa-toggle-off text-success"></i></a>`: `<a title="Activer un commentaire" href="${baseUrl}admin/comment/activate/${row.id}"><i class="fa-solid fa-toggle-on fa-xl text-danger"></i></a>`);
+                            `<a title="Désactiver le commentaire" href="<?= base_url('admin/comment/desactivate/'); ?>${row.id}"><i class="fa-solid fa-xl fa-toggle-off text-success"></i></a>`: `<a title="Activer  un commentaire" href="${baseUrl}admin/comment/activate/${row.id}"><i class="fa-solid fa-toggle-on fa-xl text-danger"></i></a>`);
                     }
                 }
             ]
